d545a2d0854344dd50660382e96f73bd523283f8
joeco fong <joecofg@gmail.com>
2020-02-26 15:17:35 -0800

    testing localhost development success

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,22 @@ import './App.css';
 class App extends Component {
 
   componentDidMount() {
-    
-    fetch("http://127.0.0.1:8080/timely/services/token", {
-      method: 'POST',
+
+
+  fetch(`http://127.0.0.1:8080/timely/services/token`, {
+      method: 'post',
+      body: JSON.stringify({
+        "userName": "test",
+        "password": "test"
+      }),
       headers: {
         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: "test",
-        password: "test",
-      })
-    })
-      .then(res => {
-        console.log(res);
-        // return res.json()
-      })
-      // .then(data => {
-      //   console.log(data);
-      // })
-  }
-
+      }
+    }).then(res => {
+      return res.json();
+    }).then(data => console.log(data));
+}
+  
 
   render() {
     return(
