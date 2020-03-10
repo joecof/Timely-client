20eefb71961d62e0c27cc978e3743040396ac07c
Dhillon-Prabh <psingh143105@gmail.com>
2020-03-10 16:56:51 -0700

    ready to commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,85 +3,81 @@ import { BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import agent from './api/agent'
 import Login from './components/Login/Login'
 import Dashboard from './components/Dashboard/Dashboard'
-import Projects from './components/ProjectsPortal/ProjectsHome/Projects';
 
 class App extends Component {
 
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  //   this.state = ({
-  //     isAuth: false,
-  //     loadedUser: null
-  //   })
+    this.state = ({
+      isAuth: false,
+      loadedUser: null
+    })
 
-  //   this.loginHandler = this.loginHandler.bind(this);
-  //   this.logoutHandler = this.logoutHandler.bind(this);
-  // }
+    this.loginHandler = this.loginHandler.bind(this);
+    this.logoutHandler = this.logoutHandler.bind(this);
+  }
 
-  // async loginHandler(event, data){
-  //   event.preventDefault();
+  async loginHandler(event, data){
+    event.preventDefault();
 
-  //   const response = await agent.authorization.login(data);
-  //   if(response) {
-  //     this.setState({
-  //       isAuth: true,
-  //       loadedUser: response
-  //     })
-  //   }
-  // }
+    const response = await agent.authorization.login(data);
+    if(response) {
+      this.setState({
+        isAuth: true,
+        loadedUser: response
+      })
+    }
+  }
 
-  // logoutHandler() {
-  //   this.setState({
-  //     isAuth: false,
-  //     loadedUser: null
-  //   })
-  // }
+  logoutHandler() {
+    this.setState({
+      isAuth: false,
+      loadedUser: null
+    })
+  }
   
   
   render() {
 
-  //   let routes = (
-  //     <Switch>
-  //       <Route
-  //         path="/"
-  //         exact
-  //         render = {props => (
-  //           <Login
-  //             {...props}
-  //             loginHandler = {this.loginHandler}
-  //           />
-  //         )}
-  //       />
-  //     </Switch>
-  //   );
+    let routes = (
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render = {props => (
+            <Login
+              {...props}
+              loginHandler = {this.loginHandler}
+            />
+          )}
+        />
+      </Switch>
+    );
 
-    // if(this.state.isAuth) {
-    //   routes = (
-    //     <Switch>
-    //       <Route
-    //         path="/dashboard"
-    //         render= {props => (
-    //           <Dashboard
-    //             {...props} 
-    //             logoutHandler = {this.logoutHandler}
-    //             loadedUser = {this.state.loadedUser}
-    //           />
-    //         )}
-    //       />
-    //     </Switch>
-    //   )
-    // }
+    if(this.state.isAuth) {
+      routes = (
+        <Switch>
+          <Route
+            path="/dashboard"
+            render= {props => (
+              <Dashboard
+                {...props} 
+                logoutHandler = {this.logoutHandler}
+                loadedUser = {this.state.loadedUser}
+              />
+            )}
+          />
+        </Switch>
+      )
+    }
     return(
-    //   <div className="App">
-    //   <BrowserRouter>
-    //     {this.state.isAuth ? <Redirect to='/dashboard/timesheet'  /> : <Redirect to='/'  />}
-    //     {routes}
-    //   </BrowserRouter>
-    // </div>
-      <div>
-        <Projects />
-      </div>
+      <div className="App">
+      <BrowserRouter>
+        {this.state.isAuth ? <Redirect to='/dashboard/timesheet'  /> : <Redirect to='/'  />}
+        {routes}
+      </BrowserRouter>
+    </div>
     )
   }
 }
