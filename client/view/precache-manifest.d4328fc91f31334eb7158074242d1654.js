d56b523a5a7bf1b516f05cda55f768b5517c1273
joecof <joecofg@gmail.com>
2020-04-08 11:33:44 -0700

    FINALLY CSS FROM OSCAR AND JOVAN

---
+++ client/view/precache-manifest.d4328fc91f31334eb7158074242d1654.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03064d3c430f9e98b99bc7e5da485b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ef089e43297456ee1e",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "31aa3d14d07bedd5eec5",
+    "url": "/static/css/main.426ea469.chunk.css"
+  },
+  {
+    "revision": "e7ef089e43297456ee1e",
+    "url": "/static/js/2.7cf827d1.chunk.js"
+  },
+  {
+    "revision": "2b725d3bcd473ec8063f1063c2e9f66f",
+    "url": "/static/js/2.7cf827d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31aa3d14d07bedd5eec5",
+    "url": "/static/js/main.a469ae4d.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "91005679a06ab3d34a9c7ec6cc16c129",
+    "url": "/static/media/logo.91005679.png"
+  },
+  {
+    "revision": "6b5281c0f60283059579534ae942d583",
+    "url": "/static/media/sideBarBackgroundImage.6b5281c0.png"
+  }
+]);
